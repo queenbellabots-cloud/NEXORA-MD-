93186b7c65fc043b5935c73e766a0d2b4c7d0af9
queenbellabots-cloud <queenbellabots@gmail.com>
2026-09-12 00:14:26 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * NEXORA MD - API Configuration
+ * API endpoints and keys ONLY. Branding lives in settings.js
+ */
+
+require('dotenv').config();
+
+global.APIs = {
+  // Filled in when downloader/media plugins are added
+};
+
+global.APIKeys = {
+  // Filled in when downloader/media plugins are added
+};
+
+module.exports = {
+  WARN_COUNT: 3
+};
